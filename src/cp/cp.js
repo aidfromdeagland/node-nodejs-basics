0c1298822c9de1716c5a0128d077719dcfb441d4
Stanislau Laniuk <aidfromdeagland@gmail.com>
2024-01-29 23:28:26 +0200

    feat: implement child_processes task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,11 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+import { fork } from 'child_process';
+
+const spawnChildProcess = async (args = []) => {
+    const dirName = fileURLToPath(new URL('.', import.meta.url));
+    const scriptPath = path.join(dirName, 'files', 'script.js');
+    const childProcess = fork(scriptPath, args, { stdio: ['inherit', 'inherit', 'inherit', 'ipc'] });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([1, 2, 3, 4, 'kek']);
