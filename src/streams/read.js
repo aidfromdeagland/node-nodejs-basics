88e8761ebdd2ee391d02117612bd7cfd6d1c3e12
Stanislau Laniuk <aidfromdeagland@gmail.com>
2024-01-29 19:49:47 +0200

    feat: implement streams tasks

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,26 @@
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+import { createReadStream } from 'fs';
+
 const read = async () => {
-    // Write your code here 
+    const dirName = fileURLToPath(new URL('.', import.meta.url));
+    const filePath = path.join(dirName, 'files', 'fileToRead.txt');
+
+    return new Promise((resolve, reject) => {
+        const readFileStream = createReadStream(filePath)
+            .on('end', () => {
+                resolve();
+            })
+            .on('error', (error) => {
+                reject(error);
+            });
+
+        readFileStream
+            .pipe(process.stdout)
+            .on('error', (error) => {
+                reject(error);
+            });
+    });
 };
 
 await read();
