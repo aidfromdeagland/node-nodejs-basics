8e1946387c6c9242e7157862d66ecb4f70e0de20
Stanislau Laniuk <aidfromdeagland@gmail.com>
2024-01-29 16:09:21 +0200

    feat: implement cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,21 @@
+
+// args.js - implement function that parses command line arguments (given in format --propName value --prop2Name value2,
+// you don't need to validate it) and prints them to the console in the format propName is value, prop2Name is value2
+
 const parseArgs = () => {
-    // Write your code here 
+    const argumentsMap = {};
+    const cliArgs = process.argv.slice(2);
+    cliArgs.forEach((arg, index, array) => {
+        if (index % 2 === 0) {
+            argumentsMap[arg.slice(2)] = array[index + 1];
+        }
+    });
+
+    const stringifiedArgs = Object.entries(argumentsMap)
+        .map(([key, value]) => `${key} is ${value}`)
+        .join(', ');
+
+    console.log(stringifiedArgs);
 };
 
 parseArgs();
