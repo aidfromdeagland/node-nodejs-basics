90d545bfacb6dac937e646f9b621c5f9f9adda66
Stanislau Laniuk <aidfromdeagland@gmail.com>
2024-01-29 22:28:55 +0200

    feat: implement worker threads tasks

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,32 @@
+import os from 'os';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+import { Worker } from 'worker_threads';
+
 const performCalculations = async () => {
-    // Write your code here
+    const dirName = fileURLToPath(new URL('.', import.meta.url));
+    const workerPath = path.join(dirName, 'worker.js');
+    const cpuCores = os.cpus().length;
+    const startNumber = 10;
+
+    const workersData = Array.from(
+        new Array(cpuCores),
+        (element, index) => {
+            return new Promise((resolve) => {
+                const handleError = () => { resolve({ status: 'error', data: null }) };
+                const worker = new Worker(workerPath, { workerData: startNumber + index });
+                worker.on('message', (result) => { resolve(result) })
+                worker.on('error', handleError)
+                worker.on('exit', (code) => {
+                    if (code !== 0) {
+                        handleError();
+                    }
+                });
+            });
+        });
+
+    const workersResults = await Promise.all(workersData);
+    console.log(workersResults);
 };
 
 await performCalculations();
