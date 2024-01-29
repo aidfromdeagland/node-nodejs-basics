ff6df827457a9e83d93bd097e51d5c3f57020303
Stanislau Laniuk <aidfromdeagland@gmail.com>
2024-01-29 21:27:28 +0200

    feat: implement zip tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
+import * as path from 'path';
+import zlib from 'zlib';
+import { fileURLToPath } from 'url';
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from 'stream/promises'
+
 const compress = async () => {
-    // Write your code here 
+    const dirName = fileURLToPath(new URL('.', import.meta.url));
+    const sourcePath = path.join(dirName, 'files', 'fileToCompress.txt');
+    const destinationPath = path.join(dirName, 'files', 'archive.gz');
+
+    await pipeline(
+        createReadStream(sourcePath),
+        zlib.createGzip(),
+        createWriteStream(destinationPath),
+    );
+
 };
 
 await compress();
